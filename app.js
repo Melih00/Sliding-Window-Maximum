ffebc7a3b3e7f34bd38b86903f1acb90dfd0e81f
Melih00 <melihagilli0@gmail.com>
2021-09-03 22:51:43 +0300

    First Commit

---
+++ app.js
@@ -0,0 +1,46 @@
+function MERHABALAR(array = [], k = 0) {
+  finalList = [];
+  for (i = 0; i < array.length - 2; i++) {
+    let newList = [];
+    let max = 0;
+    for (y = i; y < (i + k <= array.length ? i + k : array.length); y++) {
+      max = max > array[y] ? max : array[y];
+    }
+    finalList.push(max);
+  }
+  return finalList;
+}
+let listlistlist = [];
+let K = 0;
+document.getElementById("inp").addEventListener("keyup", (event) => {
+  if (event.keyCode === 13 && document.getElementById("inp").value) {
+    event.preventDefault();
+    document.getElementById("arr").innerText +=
+      document.getElementById("inp").value + ", ";
+    listlistlist.push(Number(document.getElementById("inp").value));
+    document.getElementById("inp").value = "";
+  }
+});
+document.getElementById("k_input").addEventListener("keyup", (event) => {
+  if (event.keyCode === 13 && document.getElementById("k_input").value) {
+    event.preventDefault();
+    document.getElementById("k_span").innerText =
+      document.getElementById("k_input").value;
+    K = Number(document.getElementById("k_input").value);
+    document.getElementById("k_input").value = "";
+  }
+});
+document.getElementById("GO").addEventListener("click", () => {
+  if (listlistlist && K) {
+    document.getElementById("result").innerText =
+      "[" + `${MERHABALAR(listlistlist, K)}` + "]";
+      listlistlist = []
+      K = 0;
+      document.getElementById("inp").disabled = true
+      document.getElementById("k_input").disabled = true
+  }else{
+    document.getElementById("result").innerText = 'Array veya K değeri boş bırakılamaz !'
+
+  }
+});
+document.getElementById('CLEAR').addEventListener('click', () => window.location.reload())
